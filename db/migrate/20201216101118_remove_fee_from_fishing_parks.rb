e8dea1f81bc4bfa4aecaf0c8edf4c7bd0ea8cef2
EC2 Default User <ec2-user@ip-172-31-33-206.ap-northeast-1.compute.internal>
2020-12-19 06:24:35 +0000

    レイアウト

--- db/migrate/20201216101118_remove_fee_from_fishing_parks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFeeFromFishingParks < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :fishing_parks, :fee, :integer
-  end
-end
--- db/migrate/20201216101513_remove_children_fee_from_fishing_parks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveChildrenFeeFromFishingParks < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :fishing_parks, :children_fee, :integer
-  end
-end
