9954f46f8e1a4d6270ba191dfa45776b348984fc
keisuke14 <kamatho.king13@gmail.com>
2021-02-09 02:33:02 +0000

    rspec

---
+++ spec/factories/fishing_parks.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :fishing_park do
+    # image_id {}
+    # body {}
+    # address {}
+    # name {}
+    # tour_fee {}
+    # children_tour_fee {}
+    # map_image_id {}
+    # map_body {}
+    # fish_body {}
+    # fishing_fee {}
+  end
+end
---
+++ spec/models/fishing_park_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe FishingPark, type: :model do
+  before do 
+    @fishing_park = build(:fishing_park)
+  end 
+  
+  describe "fishing_parkカラム" do
+    # it "is invalid without a image_id and body and address and name and tour_fee and children_tour_fee and map_image_id and map_body and fish_body and fishing_body"
+    # expect 
+  # pending "add some examples to (or delete) #{__FILE__}"
+  end 
+end
